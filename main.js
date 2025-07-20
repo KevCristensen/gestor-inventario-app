3b1bc2793e6c10ff3cfd096ee9ea879afeedc3f0
Kevin Cristensen <ke.cristensen@duocuc.cl>
2025-07-20 18:47:32 -0400

    Correccion total combinacion electron

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function createWindow() {
     if (app.isPackaged) {
         // Carga el index.html de Angular en producción
         mainWindow.loadURL(url.format({
-            pathname: path.join(__dirname, 'dist/angular-app/browser/index.html'),           
+            pathname: path.join(__dirname, 'dist/browser/index.html'),           
             protocol: 'file:',
             slashes: true
         }));
