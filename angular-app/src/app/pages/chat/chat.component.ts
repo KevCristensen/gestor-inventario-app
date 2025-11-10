29343d0ccbfdb6871a6fcb3c72ebb0c5b7e91d52
Kevin Cristensen <ke.cristensen@duocuc.cl>
2025-11-09 22:28:18 -0300

    Debug Chat

--- angular-app/src/app/pages/chat/chat.component.ts
+++ angular-app/src/app/pages/chat/chat.component.ts
@@ -49,13 +49,6 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
   }
 
   ngOnInit(): void {
-    this.chatService.connect();
-    // --- ¡LA SOLUCIÓN CLAVE ESTÁ AQUÍ! ---
-    // Nos aseguramos de que el usuario se "una" a la sala cada vez que el componente se carga.
-    // Esto re-registra su estado 'online' en el servidor si la conexión ya existía.
-    if (this.currentUser) {
-      this.chatService.joinChat(this.currentUser);
-    }
     this.loadUsers();
 
     // Escuchamos la lista de usuarios en línea en tiempo real
